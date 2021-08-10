a3fb11e64183da550de122656d67937f9f618bc3
Nguyen Viet Quan <quannv@solashi.com>
2021-08-10 20:55:06 +0700

    First commit

---
+++ app/Http/Controllers/SocialiteController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+
+class SocialiteController extends Controller
+{
+    public function redirect() {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function callback() {
+        $user = Socialite::driver('google')->user();
+
+        dd($user);
+    }
+}
